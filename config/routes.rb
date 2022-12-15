e8663415c4f04fdcfd77b7e89d7cd87558bbb958
joao garcia <jiboiadamor@gmail.com>
2022-12-15 13:17:58 +0000

    fixed bug in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mounnt ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 
   resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
